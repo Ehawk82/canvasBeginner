35157286fcd6aac0b1eeb55c9e800ec2d07ab802
Jason Graziano <mrehawk82@gmail.com>
2019-03-28 11:46:08 -0400

    trying

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,33 @@
-var myUI = {
+var myUI;
+
+myUI = {
 	init: () => {
 		myUI.loadout();
 	},
 	loadout: () => {
 		var dH = createEle("div");
+		var dH2 = createEle("div");
+		var dH3 = createEle("div");
+        
+        LSinit("clRect", true);
 
         dH.className = "divHolder";
+        dH2.className = "divHolder2";
+        dH3.className = "divHolder3";
 
         myUI.generateCVframe(dH);
-
+        myUI.generateCVframe2(dH2);
+        myUI.generateCVframe3(dH3);
+        
 		dvContain.append(dH);
+		dvContain.append(dH2);
+		dvContain.append(dH3);
 	},
 	generateCVframe: (dH) => {
 		var canvas = createEle("canvas"),        
             context = canvas.getContext('2d'),
             rangeToolHolder = createEle("div"),
+            togglesToolHolder = createEle("div"),
             labelAng1X = createEle("label"),
             labelAng1Y = createEle("label"),
             labelRcolor = createEle("label"),
@@ -31,14 +44,20 @@ var myUI = {
             aColorRange = createEle("input"),
             cWidthRange = createEle("input"),
             cHeightRange = createEle("input"),
-            rotateFactorRange = createEle("input");
+            clearRectToggleLabel = createEle("label"),
+            clearRectToggle = createEle("input"), clRect = loadLS("clRect");
+
+        clearRectToggleLabel.innerHTML = "CLEAR";
         
-        rotateFactorRange.type = "range";
-        rotateFactorRange.value = 0;
-        rotateFactorRange.min = -170;
-        cWidthRange.step = 1;
-        rotateFactorRange.max = 179;
-        rotateFactorRange.onchange = myUI.rotateFactorFunc(context, ang1xRange, ang1yRange, rColorRange, gColorRange, bColorRange, aColorRange, cHeightRange, cWidthRange, rotateFactorRange);
+        clearRectToggle.type = "checkbox";
+        if (clRect === "false") {
+        	clRect = false;
+        } else {
+        	clRect = true;
+        }
+        clearRectToggle.checked = clRect;
+        //alert(clRect);
+        clearRectToggle.onchange = myUI.toggleClearRectFunc(context, clearRectToggle);
 
         labelHeight.innerHTML = "HEIGHT";
         labelWidth.innerHTML = "WIDTH";
@@ -103,11 +122,9 @@ var myUI = {
 
         canvas.width = 400;
         canvas.height = 400;
-
-        context.rotate(rotateFactorRange.value * Math.PI / 180);
+        
         context.fillStyle = 'rgba(' + rColorRange.value + ',' + gColorRange.value + ',' + bColorRange.value + ',' + aColorRange.value + ')';
         context.fillRect(ang1xRange.value, ang1yRange.value, cWidthRange.value, cHeightRange.value);
-        
 
     	rangeToolHolder.className = "rangeToolHolder";
     	rangeToolHolder.append(labelAng1X);
@@ -126,33 +143,333 @@ var myUI = {
     	rangeToolHolder.append(cHeightRange);
         rangeToolHolder.append(labelWidth);
     	rangeToolHolder.append(cWidthRange);
-    	rangeToolHolder.append(rotateFactorRange);
+
+    	togglesToolHolder.append(clearRectToggleLabel);
+    	togglesToolHolder.append(clearRectToggle);
 
         dH.append(canvas);
         dH.append(rangeToolHolder);
+        dH.append(togglesToolHolder);
+	},
+	generateCVframe2: (dH2) => {
+		var canvas = createEle("canvas"),        
+            context = canvas.getContext('2d'),
+            rangeToolHolder2 = createEle("div"),
+            ang1xRange = createEle("input"), labelAng1X = createEle("label"),
+            labelAng1Y = createEle("label"),
+            ang1yRange = createEle("input"),
+            ang2xRange = createEle("input"), labelAng2X = createEle("label"),
+            labelAng2Y = createEle("label"),
+            ang2yRange = createEle("input"),
+            ang3xRange = createEle("input"), labelAng3X = createEle("label"),
+            labelAng3Y = createEle("label"),
+            ang3yRange = createEle("input");
+            
+            labelAng3X.innerHTML = "X-3";
+            labelAng3Y.innerHTML = "Y-3";
+
+            labelAng2X.innerHTML = "X-2";
+            labelAng2Y.innerHTML = "Y-2";
+
+            labelAng1X.innerHTML = "X-1";
+            labelAng1Y.innerHTML = "Y-1";
+
+            ang3yRange.type = "range";
+    	    ang3yRange.min = 1;
+        	ang3yRange.max = 399;
+        	ang3yRange.value = 100;
+        	ang3yRange.onmousemove = myUI.updateContext2(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange);
+        
+        	ang3xRange.type = "range";
+        	ang3xRange.min = 1;
+        	ang3xRange.max = 399;
+        	ang3xRange.value = 150;
+        	ang3xRange.onmousemove = myUI.updateContext2(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange);
+
+            
+            ang2yRange.type = "range";
+    	    ang2yRange.min = 1;
+        	ang2yRange.max = 399;
+        	ang2yRange.value = 310;
+        	ang2yRange.onmousemove = myUI.updateContext2(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange);
+        
+        	ang2xRange.type = "range";
+        	ang2xRange.min = 1;
+        	ang2xRange.max = 399;
+        	ang2xRange.value = 53;
+        	ang2xRange.onmousemove = myUI.updateContext2(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange);
+
+
+            ang1yRange.type = "range";
+    	    ang1yRange.min = 1;
+        	ang1yRange.max = 399;
+        	ang1yRange.value = 50;
+        	ang1yRange.onmousemove = myUI.updateContext2(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange);
+        
+        	ang1xRange.type = "range";
+        	ang1xRange.min = 1;
+        	ang1xRange.max = 399;
+        	ang1xRange.value = 50;
+        	ang1xRange.onmousemove = myUI.updateContext2(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange);
+
+
+            canvas.width = 400;
+            canvas.height = 400;
+
+            context.beginPath();
+            context.lineWidth = 2;
+            context.moveTo(50,50);
+            context.lineTo(150, 100); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(50,50);
+            context.lineTo(53, 310); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(150,100);
+            context.lineTo(53, 310); 
+            context.stroke();
+            
+            context.beginPath();
+            context.moveTo(50,50);
+    		context.lineTo(150, 100);
+    		context.fillStyle = "pink";
+    		context.lineTo(53, 310);
+    		context.fill();
+
+            rangeToolHolder2.className = "rangeToolHolder2";
+            rangeToolHolder2.append(labelAng1X);
+            rangeToolHolder2.append(ang1xRange);
+            rangeToolHolder2.append(labelAng1Y);
+            rangeToolHolder2.append(ang1yRange);
+            rangeToolHolder2.append(labelAng2X);
+            rangeToolHolder2.append(ang2xRange);
+            rangeToolHolder2.append(labelAng2Y);
+            rangeToolHolder2.append(ang2yRange);
+            rangeToolHolder2.append(labelAng3X);
+            rangeToolHolder2.append(ang3xRange);
+            rangeToolHolder2.append(labelAng3Y);
+            rangeToolHolder2.append(ang3yRange);
+
+            dH2.append(canvas);
+            dH2.append(rangeToolHolder2);
+	},
+	generateCVframe3: (dH3) => {
+		var canvas = createEle("canvas"),        
+            context = canvas.getContext('2d'),
+            rangeToolHolder3 = createEle("div"),
+            ang1xRange = createEle("input"), labelAng1X = createEle("label"),
+            labelAng1Y = createEle("label"),
+            ang1yRange = createEle("input"),
+            ang2xRange = createEle("input"), labelAng2X = createEle("label"),
+            labelAng2Y = createEle("label"),
+            ang2yRange = createEle("input"),
+            ang3xRange = createEle("input"), labelAng3X = createEle("label"),
+            labelAng3Y = createEle("label"),
+            ang3yRange = createEle("input"),
+            ang4xRange = createEle("input"), labelAng4X = createEle("label"),
+            labelAng4Y = createEle("label"),
+            ang4yRange = createEle("input");
+            
+            labelAng4X.innerHTML = "X-4";
+            labelAng4Y.innerHTML = "Y-4";
+
+            labelAng3X.innerHTML = "X-3";
+            labelAng3Y.innerHTML = "Y-3";
+
+            labelAng2X.innerHTML = "X-2";
+            labelAng2Y.innerHTML = "Y-2";
+
+            labelAng1X.innerHTML = "X-1";
+            labelAng1Y.innerHTML = "Y-1";
+
+            ang4yRange.type = "range";
+    	    ang4yRange.min = 1;
+        	ang4yRange.max = 399;
+        	ang4yRange.value = 350;
+        	ang4yRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+        
+        	ang4xRange.type = "range";
+        	ang4xRange.min = 1;
+        	ang4xRange.max = 399;
+        	ang4xRange.value = 350;
+        	ang4xRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+            
+            ang3yRange.type = "range";
+    	    ang3yRange.min = 1;
+        	ang3yRange.max = 399;
+        	ang3yRange.value = 50;
+        	ang3yRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+        
+        	ang3xRange.type = "range";
+        	ang3xRange.min = 1;
+        	ang3xRange.max = 399;
+        	ang3xRange.value = 350;
+        	ang3xRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+            
+            ang2yRange.type = "range";
+    	    ang2yRange.min = 1;
+        	ang2yRange.max = 399;
+        	ang2yRange.value = 350;
+        	ang2yRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+        
+        	ang2xRange.type = "range";
+        	ang2xRange.min = 1;
+        	ang2xRange.max = 399;
+        	ang2xRange.value = 50;
+        	ang2xRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+
+            ang1yRange.type = "range";
+    	    ang1yRange.min = 1;
+        	ang1yRange.max = 399;
+        	ang1yRange.value = 50;
+        	ang1yRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+        
+        	ang1xRange.type = "range";
+        	ang1xRange.min = 1;
+        	ang1xRange.max = 399;
+        	ang1xRange.value = 50;
+        	ang1xRange.onmousemove = myUI.updateContext3(context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange);
+
+            canvas.width = 400;
+            canvas.height = 400;
+
+            context.beginPath();
+            context.moveTo(ang1xRange.value,ang1yRange.value);
+            context.lineTo(ang2xRange.value,ang2yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang1xRange.value,ang1yRange.value);
+            context.lineTo(ang3xRange.value,ang3yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang2xRange.value,ang2yRange.value);
+            context.lineTo(ang4xRange.value,ang4yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang4xRange.value,ang4yRange.value);
+            context.lineTo(ang3xRange.value,ang3yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.fillStyle = "limegreen";
+    		context.moveTo(ang1xRange.value,ang1yRange.value);
+    		context.lineTo(ang2xRange.value,ang2yRange.value);
+    		context.lineTo(ang4xRange.value,ang4yRange.value);
+    		context.lineTo(ang3xRange.value,ang3yRange.value);
+    		context.fill();
+
+            rangeToolHolder3.className = "rangeToolHolder3";
+            rangeToolHolder3.append(labelAng1X);
+            rangeToolHolder3.append(ang1xRange);
+            rangeToolHolder3.append(labelAng1Y);
+            rangeToolHolder3.append(ang1yRange);
+            rangeToolHolder3.append(labelAng2X);
+            rangeToolHolder3.append(ang2xRange);
+            rangeToolHolder3.append(labelAng2Y);
+            rangeToolHolder3.append(ang2yRange);
+            rangeToolHolder3.append(labelAng3X);
+            rangeToolHolder3.append(ang3xRange);
+            rangeToolHolder3.append(labelAng3Y);
+            rangeToolHolder3.append(ang3yRange);
+            rangeToolHolder3.append(labelAng4X);
+            rangeToolHolder3.append(ang4xRange);
+            rangeToolHolder3.append(labelAng4Y);
+            rangeToolHolder3.append(ang4yRange);
+
+            dH3.append(canvas);
+            dH3.append(rangeToolHolder3);
 	},
 	updateContext: (context, ang1xRange, ang1yRange, rColorRange, gColorRange, bColorRange, aColorRange, cHeightRange, cWidthRange) => {
 		return () => {
         	ang1xRange.max = 399 - cWidthRange.value;
         	ang1yRange.max = 399 - cHeightRange.value;
 
-			context.clearRect(0,0,400,400);
+            var clRect = loadLS("clRect");
+            if (clRect === "false") {
 
+            } else {
+				context.clearRect(0,0,400,400);
+            }
             context.fillStyle = 'rgba(' + rColorRange.value + ',' + gColorRange.value + ',' + bColorRange.value + ',' + aColorRange.value + ')';
         	context.fillRect(ang1xRange.value, ang1yRange.value, cWidthRange.value, cHeightRange.value);
-        	context.save();
 		}
 	},
-	rotateFactorFunc: (context, ang1xRange, ang1yRange, rColorRange, gColorRange, bColorRange, aColorRange, cHeightRange, cWidthRange, rotateFactorRange) => {
+	updateContext2: (context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange) => {
 		return () => {
-        	context.save();
-        	context.clearRect(0,0,400,400);
-        	context.restore();
+        	ang1xRange.max = 399;
+        	ang1yRange.max = 399;
 
-            context.rotate(rotateFactorRange.value * Math.PI / 180);
-            context.fillStyle = 'rgba(' + rColorRange.value + ',' + gColorRange.value + ',' + bColorRange.value + ',' + aColorRange.value + ')';
-        	context.fillRect(ang1xRange.value, ang1yRange.value, cWidthRange.value, cHeightRange.value);
-        	context.save();
+		    context.clearRect(0,0,400,400);
+            
+            context.beginPath();
+            context.moveTo(ang1xRange.value,ang1yRange.value);
+            context.lineTo(ang2xRange.value,ang2yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang1xRange.value,ang1yRange.value);
+            context.lineTo(ang3xRange.value,ang3yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang2xRange.value,ang2yRange.value);
+            context.lineTo(ang3xRange.value,ang3yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.fillStyle = "pink";
+    		context.moveTo(ang1xRange.value,ang1yRange.value);
+    		context.lineTo(ang2xRange.value,ang2yRange.value);
+    		context.lineTo(ang3xRange.value,ang3yRange.value);
+    		context.fill();
+		}
+	},
+	updateContext3: (context, ang1xRange, ang1yRange, ang2xRange, ang2yRange, ang3xRange, ang3yRange, ang4xRange, ang4yRange) => {
+		return () => {
+        	ang1xRange.max = 399;
+        	ang1yRange.max = 399;
+
+		    context.clearRect(0,0,400,400);
+            
+            context.beginPath();
+            context.moveTo(ang1xRange.value,ang1yRange.value);
+            context.lineTo(ang2xRange.value,ang2yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang1xRange.value,ang1yRange.value);
+            context.lineTo(ang3xRange.value,ang3yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang2xRange.value,ang2yRange.value);
+            context.lineTo(ang4xRange.value,ang4yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.moveTo(ang4xRange.value,ang4yRange.value);
+            context.lineTo(ang3xRange.value,ang3yRange.value); 
+            context.stroke();
+
+            context.beginPath();
+            context.fillStyle = "limegreen";
+    		context.moveTo(ang1xRange.value,ang1yRange.value);
+    		context.lineTo(ang2xRange.value,ang2yRange.value);
+    		context.lineTo(ang4xRange.value,ang4yRange.value);
+    		context.lineTo(ang3xRange.value,ang3yRange.value);
+    		context.fill();
+
+		}
+	},
+	toggleClearRectFunc: (context, clearRectToggle) => {
+		return () => {
+			localStorage.setItem("clRect", clearRectToggle.checked);
 		}
 	}
 }
